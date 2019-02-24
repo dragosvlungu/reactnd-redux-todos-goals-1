61d9a2c94c910fd51350f87ca76c123687f148e9
James Priest <james-priest@outlook.com>
2019-02-23 19:37:44 -0800

    refactor: Convert strings to constants

--- index.js
+++ index.js
@@ -31,13 +31,19 @@ function createStore(reducer) {
 }
 
 // App code (reducer)
+const ADD_TODO = 'ADD_TODO';
+const REMOVE_TODO = 'REMOVE_TODO';
+const TOGGLE_TODO = 'TOGGLE_TODO';
+const ADD_GOAL = 'ADD_GOAL';
+const REMOVE_GOAL = 'REMOVE_GOAL';
+
 function todos(state = [], action) {
   switch (action.type) {
-    case 'ADD_TODO':
+    case ADD_TODO:
       return state.concat([action.todo]);
-    case 'REMOVE_TODO':
+    case REMOVE_TODO:
       return state.filter(todo => todo.id !== action.id);
-    case 'TOGGLE_TODO':
+    case TOGGLE_TODO:
       return state.map(todo =>
         todo.id !== action.id
           ? todo
@@ -50,9 +56,9 @@ function todos(state = [], action) {
 
 function goals(state = [], action) {
   switch (action.type) {
-    case 'ADD_GOAL':
+    case ADD_GOAL:
       return state.concat([action.goal]);
-    case 'REMOVE_GOAL':
+    case REMOVE_GOAL:
       return state.filter(goal => goal.id !== action.id);
     default:
       return state;
@@ -76,39 +82,39 @@ store.subscribe(() => {
 
 // dispatch an action
 store.dispatch({
-  type: 'ADD_TODO',
+  type: ADD_TODO,
   todo: { id: 0, name: 'Wash the car', complete: false }
 });
 store.dispatch({
-  type: 'ADD_TODO',
+  type: ADD_TODO,
   todo: { id: 1, name: 'Walk the dog', complete: true }
 });
 store.dispatch({
-  type: 'ADD_TODO',
+  type: ADD_TODO,
   todo: { id: 2, name: 'Go to the gym', complete: true }
 });
 store.dispatch({
-  type: 'TOGGLE_TODO',
+  type: TOGGLE_TODO,
   id: 0
 });
 store.dispatch({
-  type: 'REMOVE_TODO',
+  type: REMOVE_TODO,
   id: 0
 });
 store.dispatch({
-  type: 'ADD_GOAL',
+  type: ADD_GOAL,
   goal: { id: 0, name: 'Learn Redux' }
 });
 store.dispatch({
-  type: 'ADD_GOAL',
+  type: ADD_GOAL,
   goal: { id: 1, name: 'Learn Python' }
 });
 store.dispatch({
-  type: 'ADD_GOAL',
+  type: ADD_GOAL,
   goal: { id: 2, name: 'Learn Vue' }
 });
 store.dispatch({
-  type: 'REMOVE_GOAL',
+  type: REMOVE_GOAL,
   id: 0
 });
 
