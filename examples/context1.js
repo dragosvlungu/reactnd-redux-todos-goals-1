bf67d105765c9638ed28503f9e2ff9604ce35de2
James Priest <james-priest@outlook.com>
2019-03-13 05:46:38 -0700

    feat: Add Context API

---
+++ examples/context1.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+function Parent({ name }) {
+  return (
+    <div>
+      <h1>Parent</h1>
+      <Child name={name} />
+    </div>
+  );
+}
+
+function Child({ name }) {
+  return (
+    <div>
+      <h1>Child</h1>
+      <Grandchild name={name} />
+    </div>
+  );
+}
+
+function Grandchild({ name }) {
+  return (
+    <div>
+      <h1>Grandchild</h1>
+      <h3>Name: {name}</h3>
+    </div>
+  );
+}
+
+class App extends React.Component {
+  render() {
+    const name = 'James';
+    return <Parent name={name} />;
+  }
+}
+
+ReactDOM.render(<App />, document.getElementById('root'));
